90d4b16f91b7195b2215ceaa377fac35febc4796
temp <temp>
2016-11-28 13:32:05 -0500

    Add project files.

---
+++ Startwars/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Startwars
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel1 = new System.Windows.Forms.Label();
+            this.titleLabel2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel1
+            // 
+            this.titleLabel1.AutoSize = true;
+            this.titleLabel1.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel1.ForeColor = System.Drawing.Color.White;
+            this.titleLabel1.Location = new System.Drawing.Point(124, 9);
+            this.titleLabel1.Name = "titleLabel1";
+            this.titleLabel1.Size = new System.Drawing.Size(157, 44);
+            this.titleLabel1.TabIndex = 0;
+            this.titleLabel1.Text = "Star Wars";
+            // 
+            // titleLabel2
+            // 
+            this.titleLabel2.AutoSize = true;
+            this.titleLabel2.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel2.ForeColor = System.Drawing.Color.White;
+            this.titleLabel2.Location = new System.Drawing.Point(82, 308);
+            this.titleLabel2.Name = "titleLabel2";
+            this.titleLabel2.Size = new System.Drawing.Size(267, 44);
+            this.titleLabel2.TabIndex = 1;
+            this.titleLabel2.Text = "Go Blow Stuff Up";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(413, 374);
+            this.Controls.Add(this.titleLabel2);
+            this.Controls.Add(this.titleLabel1);
+            this.Name = "Form1";
+            this.Text = "Star Wars Animation ";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel1;
+        private System.Windows.Forms.Label titleLabel2;
+    }
+}
+
---
+++ Startwars/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Startwars
+{
+    public partial class Form1 : Form
+    {
+        Font drawFont = new Font("Arial", 12, FontStyle.Bold);
+        SolidBrush drawBrush = new SolidBrush(Color.LimeGreen);
+        Pen drawPen = new Pen(Color.White, 5);
+        int BombY;
+        int bombX = 200;
+        int x;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Graphics fg = this.CreateGraphics();
+            titleLabel1.Visible = false;
+            titleLabel2.Visible = false;
+           
+            fg.DrawString("Welcome Adveturer, your task is to destroy the\n death star. It's pretty simple  ", drawFont, drawBrush, 20, 40);
+            Thread.Sleep(1000);
+         
+            int y = 10;
+
+            while (x >= 190)
+            {
+                fg.Clear(Color.Black);
+                fg.DrawEllipse(drawPen, x, y, 50, 25);
+                fg.DrawEllipse(drawPen, 80, 75, 250, 250);
+                x = x - 1;
+
+                while (x == 190 & BombY < 200)
+                {
+                    drawBrush.Color = Color.Red;
+                    fg.Clear(Color.Black);
+                    fg.DrawEllipse(drawPen, 80, 75, 250, 250);
+                    fg.FillEllipse(drawBrush, 200, BombY, 20, 20);
+                    fg.DrawEllipse(drawPen, x, y, 50, 25);
+                    BombY++;
+                }
+                Thread.Sleep(10);
+
+            }
+            while(x >= this.Width)
+            {
+                //Make Bomb Explode 
+                fg.Clear(Color.Black);
+                fg.DrawEllipse(drawPen, x, y, 50, 25);
+                fg.DrawEllipse(drawPen, 80, 75, 250, 250);
+            }
+          
+        }
+    }
+}
