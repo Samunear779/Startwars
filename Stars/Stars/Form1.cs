a56e7d1cc854ade7b962c9371ed49f528fadf523
samne <samne@LAPTOP-K80JADTL>
2017-01-09 13:34:01 -0500

    First Commit

---
+++ Stars/Stars/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stars
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                stars();
+            }           
+        }
+
+        public void stars()
+        {
+            Random Randgen = new Random();
+            Graphics fg = this.CreateGraphics();
+            SolidBrush startBrush;
+
+            int size = Randgen.Next(1, 10);
+            int x = Randgen.Next(1, this.Width);
+            int y = Randgen.Next(1, this.Height);
+            int r = Randgen.Next(1, 256);
+            int g = Randgen.Next(1, 256);
+            int b = Randgen.Next(1, 256);
+
+            startBrush = new SolidBrush(Color.FromArgb(r, g, b));
+            fg.FillEllipse(startBrush, x, y, size, size);
+        }
+    }
+}
+
+
