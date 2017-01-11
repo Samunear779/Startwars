3a089885575881e2232e175978cf5768638b5346
samne <samne@LAPTOP-K80JADTL>
2017-01-11 13:06:35 -0500

    Added variables, working on controls screen

--- FinalDual/FinalDual/Form1.Designer.cs
+++ FinalDual/FinalDual/Form1.Designer.cs
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             this.titleLabel = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.howToPlayButton = new System.Windows.Forms.Button();
+            this.playButton = new System.Windows.Forms.Button();
             this.classSelect1 = new System.Windows.Forms.TextBox();
             this.classSelect2 = new System.Windows.Forms.TextBox();
+            this.menuButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // titleLabel
@@ -46,50 +46,60 @@
             this.titleLabel.TabIndex = 0;
             this.titleLabel.Text = "Dual";
             // 
-            // button1
+            // howToPlayButton
             // 
-            this.button1.BackColor = System.Drawing.Color.Silver;
-            this.button1.Font = new System.Drawing.Font("Papyrus", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(138, 299);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(230, 62);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "How To Play";
-            this.button1.UseVisualStyleBackColor = false;
+            this.howToPlayButton.BackColor = System.Drawing.Color.Silver;
+            this.howToPlayButton.Font = new System.Drawing.Font("Papyrus", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.howToPlayButton.Location = new System.Drawing.Point(138, 299);
+            this.howToPlayButton.Name = "howToPlayButton";
+            this.howToPlayButton.Size = new System.Drawing.Size(230, 62);
+            this.howToPlayButton.TabIndex = 1;
+            this.howToPlayButton.Text = "How To Play";
+            this.howToPlayButton.UseVisualStyleBackColor = false;
+            this.howToPlayButton.Click += new System.EventHandler(this.howToPlayButton_Click);
             // 
-            // button2
+            // playButton
             // 
-            this.button2.BackColor = System.Drawing.Color.Silver;
-            this.button2.Font = new System.Drawing.Font("Papyrus", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(615, 299);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(230, 62);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Play!";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(0, 0);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 3;
+            this.playButton.BackColor = System.Drawing.Color.Silver;
+            this.playButton.Font = new System.Drawing.Font("Papyrus", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Location = new System.Drawing.Point(615, 299);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(230, 62);
+            this.playButton.TabIndex = 2;
+            this.playButton.Text = "Play!";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
             // 
             // classSelect1
             // 
+            this.classSelect1.BackColor = System.Drawing.Color.IndianRed;
+            this.classSelect1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.classSelect1.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.classSelect1.Location = new System.Drawing.Point(741, 158);
             this.classSelect1.Name = "classSelect1";
-            this.classSelect1.Size = new System.Drawing.Size(34, 33);
+            this.classSelect1.Size = new System.Drawing.Size(34, 26);
             this.classSelect1.TabIndex = 4;
             // 
             // classSelect2
             // 
+            this.classSelect2.BackColor = System.Drawing.Color.IndianRed;
+            this.classSelect2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.classSelect2.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.classSelect2.Location = new System.Drawing.Point(741, 223);
             this.classSelect2.Name = "classSelect2";
-            this.classSelect2.Size = new System.Drawing.Size(34, 33);
+            this.classSelect2.Size = new System.Drawing.Size(34, 26);
             this.classSelect2.TabIndex = 5;
+            // 
+            // menuButton
+            // 
+            this.menuButton.BackColor = System.Drawing.Color.Silver;
+            this.menuButton.Font = new System.Drawing.Font("Papyrus", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuButton.Location = new System.Drawing.Point(12, 401);
+            this.menuButton.Name = "menuButton";
+            this.menuButton.Size = new System.Drawing.Size(240, 62);
+            this.menuButton.TabIndex = 6;
+            this.menuButton.Text = "Return To Menu ";
+            this.menuButton.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -97,11 +107,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.IndianRed;
             this.ClientSize = new System.Drawing.Size(971, 475);
+            this.Controls.Add(this.menuButton);
             this.Controls.Add(this.classSelect2);
             this.Controls.Add(this.classSelect1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.howToPlayButton);
             this.Controls.Add(this.titleLabel);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -113,11 +123,11 @@
         #endregion
 
         private System.Windows.Forms.Label titleLabel;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button howToPlayButton;
+        private System.Windows.Forms.Button playButton;
         private System.Windows.Forms.TextBox classSelect1;
         private System.Windows.Forms.TextBox classSelect2;
+        private System.Windows.Forms.Button menuButton;
     }
 }
 
